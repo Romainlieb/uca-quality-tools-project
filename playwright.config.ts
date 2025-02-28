358a95ba223924287f844718d79ad1083e3e84cd
Romain Liebert <romaiblieb@outlook.fr>
2025-02-28 19:09:02 +0100

    feat: Partie 7 ajout de la pipeline CI avec GitHub Actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     reporter: "html",
     use: {
-        baseURL: "http://localhost:3009",
         trace: "on-first-retry"
     },
     projects: [
