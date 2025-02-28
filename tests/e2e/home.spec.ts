16d40b15c14a45ee402cbfb635357e0d932db958
Romain Liebert <romaiblieb@outlook.fr>
2025-02-28 19:16:13 +0100

    feat: Partie 7 ajout de la pipeline CI avec GitHub Actions

--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -1,17 +1,11 @@
 import { test, expect } from "@playwright/test";
 
-test("Page d'accueil", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
+    // Attendre que le serveur soit disponible
     await page.goto("http://localhost:3009/");
-    console.log("Page chargée :", await page.content()); // Affiche le HTML de la page
+    await expect(page).toHaveTitle(/Super Blog/);
+});
 
-    // Vérification du titre
-    await expect(page).toHaveTitle("Super Blog");
-
-    // Vérification du contenu
-    const heading = page.locator("h1");
-    await expect(heading).toHaveText("Bienvenue sur Super Blog");
-
-    // Correction du texte du lien
-    const postsLink = page.locator("a", { hasText: "Voir la liste des articles" });
-    await expect(postsLink).toBeVisible({ timeout: 10000 });
+test("Page d'accueil", async ({ page }) => {
+    await expect(page.locator("h1")).toHaveText("Bienvenue sur Super Blog");
 });
